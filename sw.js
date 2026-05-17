05c71dca63a2fc53f8d9a32b666546f988efb621
Mia <mia.kongehl@uni-ulm.de>
2026-05-17 04:50:46 +0200

    Replace with v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fluent-cache-v7";
+const CACHE_NAME = "fluent-cache-v9";
 const ASSETS = [
   "./",
   "./index.html",
@@ -6,8 +6,7 @@ const ASSETS = [
   "./icon-192.png",
   "./icon-512.png",
   "./apple-touch-icon.png",
-  "./app-tile.png",
-  "./fluent-collage.jpg"
+  "./app-tile.png"
 ];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
